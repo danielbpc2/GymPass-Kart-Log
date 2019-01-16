2fd10003e2773cd483672f5ca74d997c4eaa2910
Daniel Carvalho <danielbpc2@hotmail.com>
2019-01-16 13:54:22 -0300

    First commit + initial files
    Includes the parser(txt to csv) I made before I forget to initiate the git

---
+++ lib/log_parser.rb
@@ -0,0 +1,20 @@
+require 'csv'
+
+def create_csv_from_txt
+  record_lines = []
+  File.open('../db/kart_log.txt', "r") do |f|
+    f.each_line do |line|
+      details = []
+      line[0] == 'H' ? puts('pular primeira linha') : details << line.split(' ')
+      details.each do |record|
+        record.delete_at(2)
+        record_lines << record
+      end
+    end
+  end
+  csv_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
+  CSV.open("../db/kart_log.csv", "wb", csv_options) do |csv|
+    csv << ["Hora", "Codigo", "Piloto", "Nº Volta", "Tempo da Volta", "Velocidade média da volta"]
+    record_lines.each {|line| csv <<  line}
+  end
+end
---
+++ lib/model/RacerRecords.rb
@@ -0,0 +1,10 @@
+class RacerRecord
+  def initialize(hora, codigo, piloto, n_volta, tempo, vel_med)
+    @hora = hora
+    @codigo = codigo
+    @piloto = piloto
+    @n_volta = n_volta
+    @tempo = tempo
+    @vel_med = vel_med
+  end
+end
